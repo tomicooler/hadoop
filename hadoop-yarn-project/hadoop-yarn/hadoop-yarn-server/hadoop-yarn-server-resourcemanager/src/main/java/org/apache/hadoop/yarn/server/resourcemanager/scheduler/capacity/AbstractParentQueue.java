6045efd6a59ed2316847fbfb997a8bfed929c110
Tamas Domok <tdomok@cloudera.com>
2023-06-06 15:33:49 +0200

    no change with the new logic (still dominant rc)
    Change-Id: I34a3e94908dd0983b4d88712f598036690b27559

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/AbstractParentQueue.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/AbstractParentQueue.java
@@ -1145,10 +1145,10 @@ public abstract class AbstractParentQueue extends AbstractCSQueue {
       ResourceLimits resourceLimits) {
     System.out.println("AbstractParentQueue.updateClusterResource " + queuePath);
 
-    if (queueContext.getConfiguration().isLegacyQueueMode()) {
-      updateClusterResourceDeprecated(clusterResource, resourceLimits);
-      return;
-    }
+    //if (queueContext.getConfiguration().isLegacyQueueMode()) {
+    //  updateClusterResourceDeprecated(clusterResource, resourceLimits);
+    //  return;
+    //}
 
     CapacitySchedulerQueueCapacityHandler handler =
         queueContext.getQueueManager().getQueueCapacityHandler();
